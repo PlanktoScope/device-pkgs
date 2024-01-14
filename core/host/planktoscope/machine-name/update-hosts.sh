ab3ea84f7fe162e5d18aaa5c94aeedfb5b380b07
Ethan Li <lietk12@gmail.com>
2024-01-14 10:31:56 -0800

    Change the location of the machine-name binary

--- core/host/planktoscope/machine-name/update-hosts.sh
+++ core/host/planktoscope/machine-name/update-hosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 serial_number="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
-machine_name="$(/home/pi/.local/bin/machine-name name --format=hex --sn="$serial_number")"
+machine_name="$(/usr/bin/machine-name name --format=hex --sn="$serial_number")"
 
 # Update /home/pi/.local/etc/hosts
 cp /home/pi/.local/etc/hosts-autogen-warning.snippet \
