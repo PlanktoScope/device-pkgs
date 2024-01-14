a97e7e372cf7af5e1183bbba0c0fa9d3736a7fbc
Ethan Li <lietk12@gmail.com>
2024-01-14 15:27:22 -0800

    Change output location of update-machine-name.sh script

--- core/host/planktoscope/machine-name/update-machine-name.sh
+++ core/host/planktoscope/machine-name/update-machine-name.sh
@@ -3,5 +3,7 @@
 serial_number="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
 machine_name="$(/usr/bin/machine-name name --format=hex --sn="$serial_number")"
 
-# Update /home/pi/.local/etc/machine-name
-echo "$machine_name" > /home/pi/.local/etc/machine-name
+# Update /var/lib/planktoscope/machine-name
+# Note: this assumes that the script is run with root permissions
+mkdir -p /var/lib/planktoscope
+printf "%s" "$machine_name" > /var/lib/planktoscope/machine-name
