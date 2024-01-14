3816b68edb5a8f8e2c8cc0b1964b7a16dabd26a6
Ethan Li <lietk12@gmail.com>
2024-01-14 15:54:05 -0800

    Reorganize files & paths for update-cockpit-origins.sh

--- core/host/planktoscope/machine-name/update-cockpit-origins.sh
+++ core/host/planktoscope/machine-name/update-cockpit-origins.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -eux
 
+script_dir="$(dirname $(realpath "$BASH_SOURCE"))"
+
 serial_number="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
 machine_name="$(/usr/bin/machine-name name --format=hex --sn="$serial_number")"
 
-# Update /home/pi/.local/etc/cockpit/origins
-cp /home/pi/.local/etc/cockpit/origins-base.snippet /home/pi/.local/etc/cockpit/origins.snippet
-sed "s/{machine-name}/$machine_name/g" /home/pi/.local/etc/cockpit/origins-machine-name.snippet \
-  >> /home/pi/.local/etc/cockpit/origins.snippet
+# Update /var/run/planktoscope/cockpit-origins
+mkdir -p /var/run/planktoscope
+cp "$(script_dir)/cockpit-origins-base" /var/run/cockpit-origins
+sed "s/{machine-name}/$machine_name/g" "$(script_dir)/cockpit-origins-machine-name" \
+  >> /var/run/planktoscope/cockpit-origins
+if [ -f "/etc/planktoscope/cockpit-origins" ]; then
+  sed "s/{machine-name}/$machine_name/g" /etc/planktoscope/cockpit-origins \
+    >> /var/run/planktoscope/cockpit-origins
+fi
 
 # Update /etc/cockpit/cockpit.conf
-origins="$(sed 's/#.*$//g' /home/pi/.local/etc/cockpit/origins.snippet | sed '/^$/d' | paste -s -d ' ')"
-sed -i "s|^Origins = .*$|Origins = ${origins}|g" /etc/cockpit/cockpit.conf
-rm /home/pi/.local/etc/cockpit/origins.snippet
+origins="$(sed 's/#.*$//g' /var/run/planktoscope/cockpit-origins | sed '/^$/d' | paste -s -d ' ')"
+sed -i "s~^Origins = .*$~Origins = ${origins}~g" /etc/cockpit/cockpit.conf
