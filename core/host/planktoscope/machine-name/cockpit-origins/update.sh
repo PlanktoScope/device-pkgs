7bf7a50470334141bcc680ecd22f97ae0f3e13e7
Ethan Li <lietk12@gmail.com>
2024-01-14 18:25:41 -0800

    Reorganize files & paths for hosts/update.sh; reorganize scripts & templates

--- core/host/planktoscope/machine-name/cockpit-origins/update.sh
+++ core/host/planktoscope/machine-name/cockpit-origins/update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -eux
 
 script_dir="$(dirname $(realpath "$BASH_SOURCE"))"
+templates_dir="$script_dir/templates"
 
 serial_number="$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number | cut -c 9-)"
 machine_name="$(/usr/bin/machine-name name --format=hex --sn="$serial_number")"
 
 # Update /var/run/planktoscope/cockpit-origins
 mkdir -p /var/run/planktoscope
-cp "$script_dir/cockpit-origins-base" /var/run/cockpit-origins
-sed "s/{machine-name}/$machine_name/g" "$script_dir/cockpit-origins-machine-name" \
+cp "$templates_dir/base" /var/run/planktoscope/cockpit-origins
+sed "s/{machine-name}/$machine_name/g" "$templates_dir/machine-name" \
   >> /var/run/planktoscope/cockpit-origins
 if [ -f "/etc/planktoscope/cockpit-origins" ]; then
   sed "s/{machine-name}/$machine_name/g" /etc/planktoscope/cockpit-origins \
