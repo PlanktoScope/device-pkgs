c7a6aa26fe1a64a9b6e63859b5cf7dd4a6e04812
Ethan Li <lietk12@gmail.com>
2024-01-15 10:26:41 -0800

    Redo IP address scheme again

--- core/host/networking/interface-forwarding/enable-interface-forwarding.sh
+++ core/host/networking/interface-forwarding/enable-interface-forwarding.sh
@@ -20,7 +20,7 @@ redirect_inbound() {
 }
 
 # Route packets between network interfaces
-joined_interfaces = (usb0 eth0 wlan0 wlan1 eth1 eth2 eth3)
+joined_interfaces = (wlan0 wlan1 eth0 eth1 eth2 eth3 usb0 usb1 usb2 usb3)
 for i in ${!joined_interfaces[@]}; do
   for j in ${!joined_interfaces[@]}; do
     if [ "$j" -le "$i" ]; then
@@ -31,10 +31,13 @@ for i in ${!joined_interfaces[@]}; do
 done
 
 # Redirect external incoming traffic bound for the device to 127.0.0.1
-redirect_inbound usb0 192.168.2.1
 redirect_inbound wlan1 192.168.3.1
 redirect_inbound wlan0 192.168.4.1
 redirect_inbound eth0 192.168.5.1
-redirect_inbound eth1 192.168.6.1
-redirect_inbound eth2 192.168.7.1
-redirect_inbound eth3 192.168.8.1
+redirect_inbound usb0 192.168.6.1
+redirect_inbound eth1 192.168.7.1
+redirect_inbound usb1 192.168.8.1
+redirect_inbound eth2 192.168.9.1
+redirect_inbound usb2 192.168.10.1
+redirect_inbound eth3 192.168.11.1
+redirect_inbound usb3 192.168.12.1
